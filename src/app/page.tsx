87f1c2c029a91e17eb3597f23ad1f305652e7649
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-07-19 14:05:54 +0530

    frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,67 @@
+"use client";
 import Image from "next/image";
-
+import { FaGithub } from "react-icons/fa";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import Gmail from "../../public/gmail.png";
+import { FaDownload } from "react-icons/fa6";
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div style={{ backgroundImage: "url('https://res.cloudinary.com/dffepahvl/image/upload/v1752875688/ifehmu2g3t6vluifruxl.jpg')", height: "100vh", backgroundRepeat: "no-repeat", backgroundSize: "cover" }} className="h-screen p-4 md:p-10 " >
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+      <div className="flex flex-row justify-between">
+
+        <h1 className="
+
+        hidden 
+        
+        
+        bg-gradient-to-r from-purple-700 via-pink-500 to-fuchsia-400 md:inline-block text-transparent bg-clip-text text-2xl md:text-5xl font-bold after:content-['</>']">
+          Akash <span></span>
+        </h1>
+
+
+        <ul className="flex flex-row gap-4 w-full md:w-fit  md:gap-8 text-white justify-evenly font-bold">
+
+
+          <li className="font-roboto text-xl md:text-2xl cursor-pointer">Home</li>
+          <li className="font-roboto text-2xl cursor-pointer">About</li>
+          <li className="font-roboto text-2xl cursor-pointer">Skills</li>
+          <li className="font-roboto text-2xl cursor-pointer">Project</li>
+          <li className="font-roboto text-2xl cursor-pointer">Contact</li>
+
+
+        </ul>
+
+
+
+
+      </div>
+      <div className="flex flex-col   mt-30">
+        <p className="text-white text-4xl xl:text-7xl font-bold">Hi , I'm Akash</p>
+
+        <p className=" font-bold text-2xl lg:text-4xl mt-8   bg-gradient-to-r from-purple-700 via-pink-500 to-fuchsia-400 md:inline-block text-transparent bg-clip-text  md:text-5xl ">Full stack developer</p>
+
+        <p className="text-white font-bold max-w-[500px]">Crafting and building modern, responsive and user-friendly web and mobile applications </p>
+
+
+
+
+
+
+        <div className="flex flex-row gap-4 items-center mt-10">
+          <button className="p-4 bg-amber-400 max-w-[300px] rounded-4xl cursor-pointer text-black font-bold flex flex-row gap-2 items-center">Download Resume <FaDownload size={18} /></button>
+          <FaGithub size={24} fill="white" className="cursor-pointer" onClick={() => window.open("https://github.com/AKASH-GPT-1678", "_blank")}/>
+          <AiOutlineLinkedin size={24} fill="white " className="cursor-pointer bg-blue-600 overflow-hidden" />
+          <Image src={Gmail} alt="gmaail" className="h-6 w-6 cursor-pointer" />
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+
+      </div>
+
+
     </div>
   );
 }
---
+++ src/app/testing/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-violet-600'>Namaste</h1>
+            <h1 className='text-purple-400'>Namaste</h1>
+
+
+    <p className='text-2xl bg-gradient-to-r from-violet-600 via-purple-500 to-pink-700 bg-clip-text text-transparent font-bold gradient-text'>My Safelty</p>
+    </div>
+  )
+}
+
+export default page
