2db3b3492be6cc93c4e4981a0f4dcc1a1c6afa0a
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-08-08 02:32:50 +0530

    all project sites

--- src/app/data/projectInfo,.ts
+++ src/app/data/projectInfo,.ts
@@ -4,6 +4,12 @@ import FleetOps from "@/app/projectassets/fleetops.png";
 import CoconutMedia from '@/app/projectassets/coconut.png';
 import LiveStock from '@/app/projectassets/livestock.png';
 import SutherLand from '@/app/projectassets/sutherland.png';
+import ChatApp from '@/app/projectassets/chatapp.png';
+import Polytika from '@/app/projectassets/polytika.png';
+import Spotify from '@/app/projectassets/spotify.png';
+
+
+
 const ProjectDetails = [
   {
     name: "FleetOps",
@@ -48,61 +54,31 @@ const ProjectDetails = [
     tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
   },
   {
-    name: "Full Stack Project Managemnt Web App",
-    image: Project.src,
-    repositoryUrl: "https://github.com/AKASH-GPT-1678/Full_Stack_Project_Management",
+    name: "ChatterBox ChatApp",
+    image: ChatApp.src,
+    repositoryUrl: "https://github.com/AKASH-GPT-1678/Full_Stack_Chatapp",
+    liveUrl: "https://full-stack-chatapp-qaoczitqi-akash-gupta-s-projects-b3af644f.vercel.app/",
+    description: "Full Stack Project Managemnt Web App and many more thena 100 of featrures to use and enjoy",
+    tools: ["Vite React", "Express", "Redis", "Mongdb", "random", "redux react"]
+  },
+  {
+    name: "Polytika Rate Politicians WebSite",
+    image: Polytika.src,
+    repositoryUrl: "https://github.com/AKASH-GPT-1678/PolyTikka",
+    liveUrl: "https://poly-tikka.vercel.app//",
+    description: "A Simple Rating Webiste for Politicians Inspired by IMDB but for leaders Built using Pure HTML and CSS and JS No Extra Frameworks",
+    tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
+  },
+  {
+    name: "Spotify Frontend Clone",
+    image: Spotify.src,
+    repositoryUrl: "https://github.com/AKASH-GPT-1678/Spotify_web",
     liveUrl: "https://rennofrontend.vercel.app/",
     description: "Full Stack Project Managemnt Web App and many more thena 100 of featrures to use and enjoy",
     tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
   },
-  {
-    name: "Full Stack Project Managemnt Web App",
-    image: Project.src,
-    repositoryUrl: "https://github.com/AKASH-GPT-1678/Full_Stack_Project_Management",
-    liveUrl: "https://rennofrontend.vercel.app/",
-    description: "Full Stack Project Managemnt Web App and many more thena 100 of featrures to use and enjoy",
-    tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
-  },
-  {
-    name: "Full Stack Project Managemnt Web App",
-    image: Project.src,
-    repositoryUrl: "https://github.com/AKASH-GPT-1678/Full_Stack_Project_Management",
-    liveUrl: "https://rennofrontend.vercel.app/",
-    description: "Full Stack Project Managemnt Web App and many more thena 100 of featrures to use and enjoy",
-    tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
-  },
-  {
-    name: "Full Stack Project Managemnt Web App",
-    image: Project.src,
-    repositoryUrl: "https://github.com/AKASH-GPT-1678/Full_Stack_Project_Management",
-    liveUrl: "https://rennofrontend.vercel.app/",
-    description: "Full Stack Project Managemnt Web App and many more thena 100 of featrures to use and enjoy",
-    tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
-  },
-  {
-    name: "Full Stack Project Managemnt Web App",
-    image: Project.src,
-    repositoryUrl: "https://github.com/AKASH-GPT-1678/Full_Stack_Project_Management",
-    liveUrl: "https://rennofrontend.vercel.app/",
-    description: "Full Stack Project Managemnt Web App and many more thena 100 of featrures to use and enjoy",
-    tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
-  },
-  {
-    name: "Coconut Media WebSite",
-    image: Project.src,
-    repositoryUrl: "https://github.com/AKASH-GPT-1678/Coconut_Media_Website",
-    liveUrl: "https://coconut-66fyhyg0o-akash-gupta-s-projects-b3af644f.vercel.app/",
-    description: "Coconut Media WebSite and many more thena 100 of featrures to use and enjoy",
-    tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
-  },
-  {
-    name: "Full Stack Project Managemnt Web App",
-    image: Project.src,
-    repositoryUrl: "https://github.com/AKASH-GPT-1678/Full_Stack_Project_Management",
-    liveUrl: "https://rennofrontend.vercel.app/",
-    description: "Full Stack Project Managemnt Web App and many more thena 100 of featrures to use and enjoy",
-    tools: ["Next.js", "express", "graphql", "oauth", "random", "redux react"]
-  }
+
+
 ];
 
 
